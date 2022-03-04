ec5cc1d8869a2170ea720182d1bb69636055012b
RyanH7279 <snoopy8911@hotmail.com>
2022-03-04 10:06:25 -0700

    Delete File Extentions directory

--- File Extentions/File Extentions.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing.sharedctypes import Value
-
-
-cases = int(input())
-total = []
-for case in range(cases):total.append(input().split(".")[1])
-p = {}
-for type in total: p[type] = total.count(type)
-for key, value in p.items(): print(key + " " + str(value))
